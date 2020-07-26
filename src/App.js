b2bbf823a17d8f2a5b60965604171af24e650e8f
batra2304 <batra2304@gmail.com>
2020-07-26 21:44:13 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          This is a React Portfolio app :D
         </p>
       </header>
     </div>
